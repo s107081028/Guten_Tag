a273c20ca148d5003761df5fbdbec1ee50a5a59e
pj-99 <62024696+pj-99@users.noreply.github.com>
2021-12-09 20:25:48 +0800

    First

---
+++ GP-Quiz_05/Assets/Scripts/Shoot.cs
@@ -0,0 +1,38 @@
+﻿using Photon.Pun;
+using UnityEngine;
+
+namespace PhotonTutorial.Weapons
+{
+    public class Shoot : MonoBehaviourPun
+    {
+        [SerializeField] private float projectileSpeed = 100f;
+        [SerializeField] public GameObject projectile;
+        [SerializeField] public GameObject spawnPoint;
+
+        private void Update()
+        {
+            if (photonView.IsMine)
+            {
+                TakeInput();
+            }
+        }
+
+        private void TakeInput()
+        {
+            if (!Input.GetMouseButtonDown(0)) { return; }
+
+            FireProjectile();
+        }
+
+        private void FireProjectile()
+        {
+            var projectileInstance = PhotonNetwork.Instantiate(
+                projectile.name,
+                spawnPoint.transform.position,
+                spawnPoint.transform.rotation
+            );
+
+            projectileInstance.GetComponent<Rigidbody>().velocity = projectileInstance.transform.forward * projectileSpeed;
+        }
+    }
+}
