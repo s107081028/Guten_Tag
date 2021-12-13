6661dce4d77aa47beb5e7c164a098354cc85e20d
s107081028 <56186644+s107081028@users.noreply.github.com>
2021-12-13 20:18:07 +0800

    test1213

--- scene_test_1129_environment/scene_test_1129_environment/Assets/Scripts/PlayerSpawner.cs
+++ scene_test_1129_environment/scene_test_1129_environment/Assets/Scripts/PlayerSpawner.cs
@@ -7,6 +7,10 @@ namespace PhotonTutorial
     {
         [SerializeField] private GameObject playerPrefab = null;
         [SerializeField] private GameObject playerCamera = null;
+        [SerializeField] private GameObject Conveyor1 = null;
+        [SerializeField] private GameObject Conveyor2 = null;
+        [SerializeField] private GameObject Teleport1 = null;
+        [SerializeField] private GameObject Teleport2 = null;
 
         private void Start()
         {
@@ -15,6 +19,10 @@ namespace PhotonTutorial
             //playerCamera.GetComponent<Transform>().position = new Vector3(player.transform.position.x, 6.5f, player.transform.position.z) 
             //- player.transform.forward*10f;
             //playerCamera.GetComponent<Transform>().LookAt(player.transform);
+            var conveyor1 = PhotonNetwork.Instantiate(Conveyor1.name, new Vector3(Conveyor1.GetComponent<ConveyorController>().x1, Conveyor1.GetComponent<ConveyorController>().y1, Conveyor1.GetComponent<ConveyorController>().z1), Quaternion.identity);
+            var conveyor2 = PhotonNetwork.Instantiate(Conveyor2.name, new Vector3(Conveyor2.GetComponent<ConveyorController>().x1, Conveyor2.GetComponent<ConveyorController>().y1, Conveyor2.GetComponent<ConveyorController>().z1), Quaternion.identity);
+            var teleport1 = PhotonNetwork.Instantiate(Teleport1.name, new Vector3(15.99f, 0.25f, 41f), Quaternion.identity);
+            var teleport2 = PhotonNetwork.Instantiate(Teleport2.name, new Vector3(-22.26f, 0.05f,-16.4f), Quaternion.identity);
         }
     }
 }
