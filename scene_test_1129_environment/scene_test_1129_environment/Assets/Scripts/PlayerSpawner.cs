0bf579ec68806b190a61a624b78027dea82d1394
pj-99 <62024696+pj-99@users.noreply.github.com>
2021-12-15 02:22:07 +0800

    TimTIm

--- scene_test_1129_environment/scene_test_1129_environment/Assets/Scripts/PlayerSpawner.cs
+++ scene_test_1129_environment/scene_test_1129_environment/Assets/Scripts/PlayerSpawner.cs
@@ -7,14 +7,24 @@ namespace PhotonTutorial
     {
         [SerializeField] private GameObject playerPrefab = null;
         [SerializeField] private GameObject playerCamera = null;
+        [SerializeField]
+        public SkinnedMeshRenderer[] renderers;
+
 
         private void Start()
         {
+
+
             var player = PhotonNetwork.Instantiate(playerPrefab.name, new Vector3(Random.Range(-20f, 10f), 1f, Random.Range(5f, 10f)), Quaternion.identity);
             playerCamera = GameObject.Find("Main Camera");
             playerCamera.GetComponent<Transform>().position = new Vector3(player.transform.position.x, 6.5f, player.transform.position.z) 
             - player.transform.forward*10f;
             playerCamera.GetComponent<Transform>().LookAt(player.transform);
+
+            foreach (SkinnedMeshRenderer renderer in renderers)
+            {
+                renderer.enabled = false;
+            }
         }
     }
 }
