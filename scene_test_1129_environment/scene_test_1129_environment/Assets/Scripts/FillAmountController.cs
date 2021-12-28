038dde458a16dff53aa14dcfc55a26c1a30d99ee
icysushi <timclock9324@gmail.com>
2021-12-28 20:22:23 +0800

    sync bar

--- scene_test_1129_environment/scene_test_1129_environment/Assets/Scripts/FillAmountController.cs
+++ scene_test_1129_environment/scene_test_1129_environment/Assets/Scripts/FillAmountController.cs
@@ -29,6 +29,8 @@ public class FillAmountController : MonoBehaviour
     private SkillController skillController;
     private PhotonView photonView;
 
+    public Sprite charactor_skill_sprite;
+
 
     public Image[] bulletPicked = new Image[6];
     // public GameObject player;
@@ -36,7 +38,7 @@ public class FillAmountController : MonoBehaviour
     void Start()
     {
 
-
+        Image skill1_Image = GameObject.Find("SkillUI/SkillCoolDown/Skill1").GetComponent<Image>();
         skill1CoolDownImage = GameObject.Find("SkillUI/SkillCoolDown/Skill1/Skill1CoolDown").GetComponent<Image>();
         skill2CoolDownImage = GameObject.Find("SkillUI/SkillCoolDown/Skill2/Skill2CoolDown").GetComponent<Image>();
         //skill3CoolDownImage = GameObject.Find("SkillAndSprintCanvas/SkillCoolDown/Skill3/Skill3CoolDown").GetComponent<Image>();
@@ -47,11 +49,13 @@ public class FillAmountController : MonoBehaviour
         
      //   sprintPowerImage = GameObject.Find("SkillAndSprintCanvas/bgArtboard 17").GetComponent<Image>();
     
-        bulletPicked[0] = GameObject.Find("SkillAndSprintCanvas/Orange theme spritesheet 1_0").GetComponent<Image>();
-        bulletPicked[1] = GameObject.Find("SkillAndSprintCanvas/Orange theme spritesheet 1_0 (1)").GetComponent<Image>();
-        bulletPicked[2] = GameObject.Find("SkillAndSprintCanvas/Orange theme spritesheet 1_0 (2)").GetComponent<Image>();
-        bulletPicked[3] = GameObject.Find("SkillAndSprintCanvas/Orange theme spritesheet 1_0 (3)").GetComponent<Image>();
-        bulletPicked[4] = GameObject.Find("SkillAndSprintCanvas/Orange theme spritesheet 1_0 (4)").GetComponent<Image>();
+        bulletPicked[0] = GameObject.Find("SkillUI/SkillCoolDown/bullet1").GetComponent<Image>();
+        bulletPicked[1] = GameObject.Find("SkillUI/SkillCoolDown/bullet2").GetComponent<Image>();
+        bulletPicked[2] = GameObject.Find("SkillUI/SkillCoolDown/bullet3").GetComponent<Image>();
+        bulletPicked[3] = GameObject.Find("SkillUI/SkillCoolDown/bullet4").GetComponent<Image>();
+        bulletPicked[4] = GameObject.Find("SkillUI/SkillCoolDown/bullet5").GetComponent<Image>();
+
+        skill1_Image.sprite = charactor_skill_sprite;
         RefreshBulletNum(0);
     }
 
