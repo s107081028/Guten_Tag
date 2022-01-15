3099901321c47c9f46f03beeadf2bc5b63e323b7
pj-99 <62024696+pj-99@users.noreply.github.com>
2022-01-15 09:18:09 +0800

    000

--- scene_test_1129_environment/scene_test_1129_environment/Assets/Scripts/Menu/ChangeScene.cs
+++ scene_test_1129_environment/scene_test_1129_environment/Assets/Scripts/Menu/ChangeScene.cs
@@ -22,4 +22,8 @@ public class ChangeScene : MonoBehaviour
     public void LoadScene() {
         SceneManager.LoadScene(targetScene);
     }
+
+    public void CloseGame() {
+        Application.Quit();
+    }
 }
