e57d6e1131d1ee552033629b1613ab64ce453f3b
pj-99 <62024696+pj-99@users.noreply.github.com>
2022-01-18 04:10:44 +0800

    timmy0410

--- scene_test_1129_environment/scene_test_1129_environment/Assets/Scripts/TeleportController.cs
+++ scene_test_1129_environment/scene_test_1129_environment/Assets/Scripts/TeleportController.cs
@@ -9,11 +9,17 @@ public class TeleportController : MonoBehaviourPun
     public float lastEnterTime;
     public float coolTime;
     public Dictionary<string , float> dict = new Dictionary<string,  float>();
+
+    public GameObject teleportEffect;
+    public ParticleSystem teleportParticalEffect;
     // Start is called before the first frame update
     void Start()
     {
         lastEnterTime = Time.time;
         coolTime = 5.0f;
+        if (TryGetComponent(out ParticleSystem ps)) {
+            teleportParticalEffect = ps;
+        }
     }
 
     // Update is called once per frame
@@ -31,12 +37,16 @@ public class TeleportController : MonoBehaviourPun
                 dict.Add(Collision.gameObject.GetComponent<PhotonView>().Owner.UserId, Time.time);
                 AnotherTeleport.GetComponent<TeleportController>().dict.Add(Collision.gameObject.GetComponent<PhotonView>().Owner.UserId, Time.time);
                 Collision.transform.position = AnotherTeleport.transform.position;
+                hideTeleportEffect();
+                Invoke(nameof(showTeleportEffect), coolTime);
             }
             else if (Time.time - dict[Collision.gameObject.GetComponent<PhotonView>().Owner.UserId] >= coolTime)
             {
                 dict[Collision.gameObject.GetComponent<PhotonView>().Owner.UserId] = Time.time;
                 AnotherTeleport.GetComponent<TeleportController>().dict[Collision.gameObject.GetComponent<PhotonView>().Owner.UserId] = Time.time;
                 Collision.transform.position = AnotherTeleport.transform.position;
+                hideTeleportEffect();
+                Invoke(nameof(showTeleportEffect), coolTime);
             }
             /*
             if (!dict.ContainsKey(Collision.gameObject.name)) {
@@ -50,6 +60,33 @@ public class TeleportController : MonoBehaviourPun
                 AnotherTeleport.GetComponent<TeleportController>().dict[Collision.gameObject.name] = Time.time;
                 Collision.transform.position = AnotherTeleport.transform.position;
             }      */
+        }
+    }
+
+    void showTeleportEffect() {
+        if (teleportEffect && AnotherTeleport.GetComponent<TeleportController>().teleportEffect) {
+            teleportEffect.SetActive(true);
+            AnotherTeleport.GetComponent<TeleportController>().teleportEffect.SetActive(true);
+        }
+
+        if (teleportParticalEffect && AnotherTeleport.GetComponent<TeleportController>().teleportParticalEffect)
+        {
+            teleportParticalEffect.Play();
+            AnotherTeleport.GetComponent<TeleportController>().teleportParticalEffect.Play();
+        }
+        
+    }
+
+    void hideTeleportEffect() {
+        if (teleportEffect && AnotherTeleport.GetComponent<TeleportController>().teleportEffect){
+            teleportEffect.SetActive(false);
+            AnotherTeleport.GetComponent<TeleportController>().teleportEffect.SetActive(false);
+        }
+
+        if (teleportParticalEffect && AnotherTeleport.GetComponent<TeleportController>().teleportParticalEffect)
+        {
+            teleportParticalEffect.Stop();
+            AnotherTeleport.GetComponent<TeleportController>().teleportParticalEffect.Stop();
         }
     }
 
